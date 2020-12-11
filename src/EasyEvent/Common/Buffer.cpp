ed76ceb704730bcb4d95a2fd50fa7d778917479a
yuwenyong <136002018@qq.com>
2020-12-11 12:58:52 +0800

    Add buffer

--- src/EasyEvent/Common/Buffer.cpp
+++ src/EasyEvent/Common/Buffer.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "EasyEvent/Common/Buffer.h"
+
+
+void EasyEvent::Buffer::prepare(size_t size) {
+    if (getRemainingSpace() < size) {
+        normalize();
+        if (getRemainingSpace() < size) {
+            size_t newSize = _capacity * 2;
+            if (newSize < getActiveSize() + size) {
+                newSize = getActiveSize() + size;
+            }
+            _storage = (uint8*)::realloc(_storage, newSize);
+            _capacity = newSize;
+        }
+    }
+}
--- src/EasyEvent/Common/Buffer.h
+++ src/EasyEvent/Common/Buffer.h
@@ -6,18 +6,124 @@
 #define EASYEVENT_COMMON_BUFFER_H
 
 #include "EasyEvent/Common/Config.h"
+#include "EasyEvent/Common/NonCopyable.h"
 
 
 namespace EasyEvent {
 
-    class EASY_EVENT_API Buffer {
+    class EASY_EVENT_API Buffer: private NonCopyable {
     public:
+        Buffer()
+            : Buffer(4096) {
 
-    protected:
+        }
+
+        explicit Buffer(size_t capacity)
+            : _capacity(capacity) {
+            _storage = (uint8*)malloc(capacity);
+        }
+
+        Buffer(Buffer&& rhs) noexcept {
+            _storage = rhs._storage;
+            _capacity = rhs._capacity;
+            _wpos = rhs._wpos;
+            _rpos = rhs._rpos;
+            rhs._storage = nullptr;
+            rhs._capacity = 0;
+            rhs.reset();
+        }
+
+        Buffer& operator=(Buffer&& rhs) noexcept {
+            if (_storage) {
+                ::free(_storage);
+            }
+            _storage = rhs._storage;
+            _capacity = rhs._capacity;
+            _wpos = rhs._wpos;
+            _rpos = rhs._rpos;
+
+            rhs._storage = nullptr;
+            rhs._capacity = 0;
+            rhs.reset();
+            return *this;
+        }
+
+        ~Buffer() {
+            if (_storage) {
+                ::free(_storage);
+            }
+        }
+
+        void reset() {
+            _wpos = 0;
+            _rpos = 0;
+        }
+
+        void prepare(size_t size);
+
+        void normalize() {
+            if (_rpos && _rpos != _wpos) {
+                memmove(getBasePointer(), getReadPointer(), getActiveSize());
+                _wpos -= _rpos;
+                _rpos = 0;
+            }
+        }
+
+        uint8* getBasePointer() {
+            Assert(_storage != nullptr);
+            return _storage;
+        }
+
+        uint8* getReadPointer() {
+            return getBasePointer() + _rpos;
+        }
+
+        uint8* getWritePointer() {
+            return getBasePointer() + _wpos;
+        }
+
+        void readCompleted(size_t bytes) {
+            _rpos += bytes;
+            Assert(_capacity >= _rpos);
+        }
+
+        void writeCompleted(size_t bytes) {
+            _wpos += bytes;
+            Assert(_capacity >= _wpos);
+        }
+
+        size_t getActiveSize() const {
+            Assert(_wpos >= _rpos);
+            return _wpos - _rpos;
+        }
+
+        size_t getRemainingSpace() const {
+            Assert(_capacity >= _wpos);
+            return _capacity - _wpos;
+        }
+
+        void write(void const* data, size_t size) {
+            if (size) {
+                prepare(size);
+                ::memcpy(getWritePointer(), data, size);
+                writeCompleted(size);
+            }
+        }
+
+        size_t read(void* buf, size_t bufSize) {
+            size_t size = bufSize < getActiveSize() ? bufSize : getActiveSize();
+            if (size) {
+                ::memcpy(buf, getReadPointer(), size);
+                readCompleted(size);
+            }
+            return size;
+        }
+
+    private:
         uint8* _storage;
         size_t _capacity;
-        size_t _wpos;
-        size_t _rpos;
+        size_t _wpos{0};
+        size_t _rpos{0};
     };
 
 }
